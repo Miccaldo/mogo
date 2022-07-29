dd2bbafe748312244aa536d80f0aea401737a581
Michał <m.janowski@global4net.com>
2022-07-29 12:08:04 +0200

    Add header, main, footer init root components.

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,8 +1,14 @@
 import './app.scss';
+import Header from "../Header";
+import Main from "../Main";
+import Footer from "../Footer";
 
 function App() {
   return (
     <div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
